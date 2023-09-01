3e4c62a4bd2a2c5d0e3889cd45e609a0e3805ab4
Qulip <alexryu1105@naver.com>
2023-09-01 18:00:29 +0900

    feat: videojs-overlay 추가

--- static/js/index.js
+++ static/js/index.js
@@ -7,11 +7,28 @@ const player = videojs("myVideo", {
   preload: "metadata",
 });
 
+let overlaysData = [
+  {
+    content: "영상 속 여자가 입은 자켓이 궁금하면?",
+    start: 3,
+    end: 6,
+  },
+  {
+    content: "영상 속 여자의 휴대폰이 궁금하면?",
+    start: 9,
+    end: 12,
+  },
+];
+
+player.overlay({
+  overlays: overlaysData,
+});
+
 player.on("timeupdate", () => {
   const currentTime = player.currentTime(); // 현재 진행 시간
   // const duration = player.duration(); // 총 길이
 
-  document.getElementById("t1").textContent = "현재 시간 : " + currentTime.toFixed(0);
+  document.getElementById("time").innerText = "현재 시간 : " + currentTime.toFixed(0);
 
   if (currentTime.toFixed(0) == 4) {
     document.getElementById("ad").style.display = "block";
