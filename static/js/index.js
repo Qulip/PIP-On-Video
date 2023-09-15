e5a869d2e2c12ad5bfb24c08f349a74600bccd50
Qulip <alexryu1105@naver.com>
2023-09-15 16:58:58 +0900

    feat: 광고 장면 수정

--- static/js/index.js
+++ static/js/index.js
@@ -16,15 +16,34 @@ let overlaysData = [
     content: "전현무가 착용하고 있는 헤드셋이 궁금하다면?",
     start: 15,
     end: 25,
-    text: "<div class='adListName'>에어팟 맥스</div>",
-    url: "<a class='adListLink href='https://www.apple.com/kr/airpods-max/'>바로가기</a>",
+    title: "<div class='adListName'>▶에어팟 맥스</div>",
+    text: "<div class='adListText'>감동적인 하이파이 오디오와 AirPods 특유의 매혹적인 사용성의 완벽한 조합.</div>",
+    image:
+      "<div class='adListImageDiv' title='자세히 알아보기' onclick='window.open(`https://www.apple.com/kr/airpods-max/`)'>" +
+      "<image class='adListImage' src='./static/img/airpods_max.jpg'></image>" +
+      "</div>",
   },
   {
     content: "전현무가 간 바다가 궁금하다면??",
     start: 37,
     end: 51,
-    text: "<div class='adListName'>탄도항</div>",
-    url: "<a class='adListLink href='#'>바로가기</a>",
+    title: "<div class='adListName'>▶탄도항</div>",
+    text: "<div class='adListText'>하루 두 번의 썰물 때 탄도와 누에섬 사이에 갯벌이 드러나 바다가 갈라지는 신비한 현상을 볼 수 있다.</div>",
+    image:
+      "<div class='adListImageDiv' title='자세히 알아보기' onclick='window.open(`https://www.ggtour.or.kr/tourdb/goosuk.php?tmenu=&smenu=&stitle=&tsort=2&msort=15&board_code=71&board=71&s_category_name=&s_view_yn=&key=%ED%83%84%EB%8F%84%ED%95%AD&no=9571&mode=detail&page=1&s_tag=&s_admin_no=`)'>" +
+      "<image class='adListImage' src='./static/img/tando.jpeg'></image>" +
+      "</div>",
+  },
+  {
+    content: "이장우가 마시는 물이 궁금하다면??",
+    start: 57,
+    end: 67,
+    title: "<div class='adListName'>▶삼다수</div>",
+    text: "<div class='adListText'>화산송이가 키운 생명력이 물 속에서 몸 속까지</div>",
+    image:
+      "<div class='adListImageDiv' title='자세히 알아보기' onclick='window.open(`https://www.coupang.com/vp/products/5647481827?itemId=19365622542&vendorItemId=86478559175&pickType=COU_PICK&q=%EC%82%BC%EB%8B%A4%EC%88%98&itemsCount=36&searchId=487bf5a96f8c4885a73bdcf456655934&rank=1&isAddedCart=`)'>" +
+      "<image class='adListImage' src='./static/img/samdasu.jpg'>" +
+      "</image></div>",
   },
 ];
 
@@ -46,9 +65,11 @@ document
   });
 
 $(() => {
-  let adText = "<div id='adListHeader'>전체 광고 모음</div>";
+  let adText = "<div class='adListHeader'>전체 Tag 모음</div>";
+  let adIdx = 0;
   overlaysData.forEach((data) => {
-    adText = adText + data.text + data.url;
+    adText = adText + "<div class='adListItem' id='adListItem" + adIdx + "'>" + data.title + data.image + data.text + "</div>";
+    adIdx++;
   });
   $("#adList").html(adText);
   console.log(adText);
@@ -58,15 +79,16 @@ player.on("timeupdate", () => {
   const currentTime = player.currentTime();
 
   if (idx != overlaysData.length) {
-    console.log(currentTime.toFixed(3) + " " + idx);
     if (show == 0 && Math.floor(currentTime) == overlaysData[idx].start) {
       show = 1;
       $("#overlayText").html(overlaysData[idx].content);
-      $("#nowAd").html(overlaysData[idx].text + overlaysData[idx].url);
+      $("#adListItem" + idx).addClass("hidden");
+      $("#nowAd").html("<div class='adListHeader'>지금 나오는 Tag</div>" + overlaysData[idx].title + overlaysData[idx].text + overlaysData[idx].image);
     }
     if (show == 1 && Math.floor(currentTime) == overlaysData[idx].end) {
       show = 0;
       $("#overlayText").html("&nbsp;!&nbsp;");
+      $("#adListItem" + idx).removeClass("hidden");
       $("#nowAd").html("");
       idx++;
     }
